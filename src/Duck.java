fc4a96067ee172289bf23ac9e8cd9b2fcaac4731
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-05-31 10:51:35 +0200

    feat: avoiding nullPointerException using conditional opertor

---
+++ src/Duck.java
@@ -0,0 +1,24 @@
+
+public class Duck {
+    private String name;
+    private Integer age;
+
+    public Duck() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        var duck = new Duck();
+        duck.setName("YELLOW DUCK");
+        duck.setAge(10);
+        duck = null;
+        // throw is NullPointerException
+        /*
+        if(duck != null & duck.getAge()<5 ){
+            System.out.println("Duck-> "+ duck.getName());
+        }
+         */
+
+        //Conditional operator throw is NullPointerException
+        if (duck != null && duck.getAge() < 5) {
+            System.out.println("Duck-> " + duck.getName());
+        }
+
+
+    }
+}
