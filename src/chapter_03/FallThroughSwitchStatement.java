fde80ccb5e4e0021582d38725f2226335977b5e3
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-05-31 17:25:29 +0200

    feat: implement switch statements 2nd example

--- src/chapter_03/FallThroughSwitchStatement.java
+++ src/chapter_03/FallThroughSwitchStatement.java
@@ -5,22 +5,41 @@ public class FallThroughSwitchStatement {
         String day = "Friday";
 
         switch (day) {
-            case "Monday":
-            case "Tuesday":
-            case "Wednesday":
-            case "Thursday":
+            case "Monday", "Tuesday", "Wednesday", "Thursday":
                 System.out.println(day + " is a workday.");
                 break;
             case "Friday":
                 System.out.println(day + " is a study day.");
                 break;
-            case "Saturday":
-            case "Sunday":
+            case "Saturday", "Sunday":
                 System.out.println(day + " is a weekend.");
                 break;
             default:
                 System.out.println("Invalid day.");
                 break;
         }
+
+
+        // season of the year
+        String month = "April";
+
+        switch (month) {
+            case "December", "January", "February":
+                System.out.println(month + "is in the Winter");
+            case "March", "April", "May":
+                System.out.println(month + "is in the Spring");
+            case "June", "July", "August":
+                System.out.println(month + "is in the Summer");
+                break;
+            case "September", "October", "November":
+                System.out.println(month + "is in the Fall");
+            default:
+                System.out.println("unknown season");
+                break;
+        };
+
     }
 }
+
+
+
