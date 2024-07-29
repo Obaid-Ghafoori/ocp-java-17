68185dabe392bbc94ba16b3c67d2ccfa1e33bd6e
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-07-29 20:07:34 +0200

    refactor: assign major subject for student

--- src/chapter_08/student_grade_filter/StudentSupplier.java
+++ src/chapter_08/student_grade_filter/StudentSupplier.java
@@ -7,19 +7,21 @@ import java.util.function.Supplier;
 public class StudentSupplier implements Supplier<Student> {
     private String[] firstNames = {"John", "Emily", "Michael", "Sarah", "William", "Olivia", "James", "Ava", "George", "Isabella"};
     private String[] lastNames = {"Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor"};
+    private String[] majors = {"Science", "IT", "Mathematics", "Business", "Administration"};
     private int count = 0;
 
     @Override
     public Student get() {
         String firstName = firstNames[count % firstNames.length];
         String lastName = lastNames[count % lastNames.length];
+        String major = majors[count % majors.length];
         String name = firstName + " " + lastName;
         double grade = 80.0 + (count % 10);
-        String major = "Science";
+        String specialization  = major;
 
         count++;
 
-        return new Student(name, grade, major);
+        return new Student(name, grade, specialization);
     }
 }
 
