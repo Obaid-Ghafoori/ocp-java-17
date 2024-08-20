d5a6a00c16bc718fc76e2e30020ef30f72d81d54
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-08-20 23:29:54 +0200

    feat: add repository and its implementation

---
+++ src/chapter_10/social_media_analyzer/domain/UserRepository.java
@@ -0,0 +1,41 @@
+package chapter_10.social_media_analyzer.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Interface for accessing user data.
+ */
+public interface UserRepository {
+
+    /**
+     * Retrieves all users.
+     *
+     * @return a list of all users
+     */
+    List<User> findAll();
+
+    /**
+     * Retrieves a user by their unique identifier.
+     *
+     * @param userId the unique identifier of the user
+     * @return an Optional containing the user if found, or an empty Optional if not
+     */
+    Optional<User> findById(String userId);
+
+    /**
+     * Saves a new or existing user.
+     *
+     * @param user the user to be saved
+     * @return the saved user
+     */
+    User save(User user);
+
+    /**
+     * Deletes a user by their unique identifier.
+     *
+     * @param userId the unique identifier of the user to be deleted
+     */
+    void deleteById(String userId);
+}
+
---
+++ src/chapter_10/social_media_analyzer/infrastructure/UserRepositoryImpl.java
@@ -0,0 +1,45 @@
+package chapter_10.social_media_analyzer.infrastructure;
+
+import chapter_10.social_media_analyzer.domain.User;
+import chapter_10.social_media_analyzer.domain.UserRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * In-memory implementation of the UserRepository interface.
+ */
+public class UserRepositoryImpl implements UserRepository {
+    private final Map<String, User> userDatabase = new HashMap<>();
+
+    @Override
+    public List<User> findAll() {
+        return userDatabase.values().stream().collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<User> findById(String userId) {
+        return Optional.ofNullable(userDatabase.get(userId));
+    }
+
+    @Override
+    public User save(User user) {
+        if (userDatabase.containsKey(user.userId())) {
+            throw new IllegalArgumentException(String.format("User ID already exists: %s", user.userId()));
+        }
+        userDatabase.put(user.userId(), user);
+        return user;
+    }
+
+    @Override
+    public void deleteById(String userId) {
+        if (!userDatabase.keySet().contains(userId)) {
+            throw new IllegalArgumentException(String.format("User ID %s does not exists: ", userId));
+        }
+        userDatabase.remove(userId);
+
+    }
+}
