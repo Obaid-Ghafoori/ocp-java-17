743a0d5cb3d153d025584852b7db68b4e2327722
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-08-31 14:32:45 +0200

    feat: create inventory management maven proj

---
+++ src/chapter_14/InventoryManagementSystem/src/main/java/com/inventory/App.java
@@ -0,0 +1,13 @@
+package com.inventory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
