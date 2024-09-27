70f7e0b5fc715f0c0d5ce17ffb8b04fdf5d827f3
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-09-27 17:55:08 +0200

    refactor: refactor

--- src/chapter_13/SheepManagerWithLock.java
+++ src/chapter_13/SheepManagerWithLock.java
@@ -2,7 +2,7 @@ package chapter_13;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SheepManagerWithLock {
@@ -10,12 +10,21 @@ public class SheepManagerWithLock {
     private final ReentrantLock lock = new ReentrantLock();
 
     public void incrementAndReport() {
-        lock.lock();
         try {
-            System.out.print((++counter) + " ");
-        } finally {
-            lock.unlock();
+            if(lock.tryLock(100, TimeUnit.MILLISECONDS)){
+                try {
+                    System.out.print((++counter) + " ");
+                    System.out.println("Lock obtained, entering protecting coding block");
+                } finally {
+                    lock.unlock();
+                }
+            }else {
+                System.out.println(" Unable to acquire lock, doing something else at the moment");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
     }
 
     public static void main(String[] args) {
