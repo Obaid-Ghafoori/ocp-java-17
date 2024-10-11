c44e9273bb4247e1ed0de97ff4116588568acca2
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-10-11 13:15:41 +0200

    feat: copy file using fileManager

--- src/chapter_14/file_management_system/src/main/FileManager.java
+++ src/chapter_14/file_management_system/src/main/FileManager.java
@@ -1,4 +1,27 @@
 package chapter_14.file_management_system.src.main;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * Class responsible for managing file operations like copy, move, and delete.
+ */
 public class FileManager {
+
+    /**
+     * Copies a file from source to destination.
+     *
+     * @param source      the source file path
+     * @param destination the destination file path
+     * @throws IOException if the file cannot be copied
+     */
+
+    public void copyFile(Path source, Path destination) throws IOException{
+        FileValidator.validateFile(source);
+        Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+        System.out.println(String.format("File copied from: %s to %s", source, destination));
+
+    }
 }
